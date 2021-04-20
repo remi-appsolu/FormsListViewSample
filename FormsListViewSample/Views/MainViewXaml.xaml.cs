ddb97a07e24c3d6dd9f8d5bca10516787c4e615f
remi <remi.brun@appsolu.net>
2021-04-20 23:12:23 +0200

    Ajoutez des fichiers projet.

---
+++ FormsListViewSample/Views/MainViewXaml.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
+
+namespace FormsListViewSample
+{
+	
+	public partial class MainViewXaml : ContentPage
+	{
+		public ObservableCollection<VeggieViewModel> veggies { get; set; }
+        public MainViewXaml()
+        {
+            InitializeComponent();
+
+            veggies = new ObservableCollection<VeggieViewModel>();
+            for (int i = 0; i < 20; i++) {
+                veggies.Add(new VeggieViewModel { Name = "Tomato", Type = "Fruit", Image = "tomato.png" });
+                veggies.Add(new VeggieViewModel { Name = "Romaine Lettuce", Type = "Vegetable", Image = "lettuce.png" });
+                veggies.Add(new VeggieViewModel { Name = "Zucchini", Type = "Vegetable", Image = "zucchini.png" });
+            }
+            lstView.ItemsSource = veggies;
+            refreshButton.Clicked += RefreshButton_Clicked;
+            memoryStatusLabel.Text = "Memory used : " + GC.GetTotalMemory(true);
+
+        }
+
+
+        private void RefreshButton_Clicked(object sender, System.EventArgs e)
+        {
+            lstView.ItemsSource = veggies;
+            lstView.ScrollTo(veggies[0], ScrollToPosition.MakeVisible, true);
+            // For simplicity, but simple Console.WriteLine will show the memory usage growing constantly
+            // Console.WriteLine("Memory used : " + GC.GetTotalMemory(true));
+            // 
+            memoryStatusLabel.Text = "Memory used : " + GC.GetTotalMemory(true);
+        }
+    }
+}
+
